c850086322dc782f49232b8b53865bf8d30bcd8f
Nessa9708 <37573204+Nessa9708@users.noreply.github.com>
2023-08-24 23:17:05 +0200

    Update getProducts.js

--- functions/getProducts.js
+++ functions/getProducts.js
@@ -1,6 +1,4 @@
 const sanityClient = require('@sanity/client');
-const imageURLBuilder = require('@sanity/image-url');
-const blocksToHTML = require('@sanity/block-content-to-html');
 
 const sanity = sanityClient({
     projectId: process.env.SANITY_PROJECT_ID,
@@ -10,19 +8,17 @@ const sanity = sanityClient({
 
 exports.handler = async () => {
     const query = '*[+type=="product"] | order(title asc)';
-    const products = await sanity.fetch(query).then((results) => {
-        const allProducts = results.map((product) => {
-            const output ={
-                id: project.slug.current,
-                name: project.title,
-                url: `${process.env.URL}/.netlify/functions/getProducts`,
-            }
-            return output;
-        });
-        return allProducts;
-    });
-    return {
-        statusCode: 200,
-        body: 'ok',
-    };
-}
+    try {
+        const products = await sanity.fetch(query);
+        return {
+            statusCode: 200,
+            body: JSON.stringify(products), // Return the fetched products as JSON
+        };
+    } catch (error) {
+        console.error('Error fetching products:', error);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: 'Internal Server Error' }),
+        };
+    }
+};
